2f213930ec64876f2fdb16b471c985f40b5269c4
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-12-12 21:48:15 -0500

    delete all from dev branch

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  # Root route
-  root "api/greetings#random_greeting"
-
-  # API namespace
-  namespace :api do
-    get 'random_greeting', to: 'greetings#random_greeting'
-  end
-end
