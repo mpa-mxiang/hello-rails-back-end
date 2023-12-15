444baa1074a1d03726e60bd61fce38e5fc3e2ecb
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-12-14 23:22:52 -0500

    add cors middleware

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,11 @@ module HelloRailsBackEnd
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3001' # Add the origin of your React app
+        resource '/api/*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
   end
 end
